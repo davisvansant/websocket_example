753e956707777256bda7243c8b2af02f7b8ccb32
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-03-10 11:17:42 -0800

    initial (non ws) server, to iterate off of

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use actix::{Actor, StreamHandler};
+use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer};
+use actix_web_actors::ws;
+
+async fn index() -> Result<HttpResponse, Error> {
+    let mut builder = actix_web::dev::HttpResponseBuilder::new(actix_web::http::StatusCode::OK);
+    let body = actix_web::dev::Body::from("Hi from websockets!");
+    let response = builder.body(body);
+    println!("{:?}", response);
+    Ok(response)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/ws/", web::get().to(index)))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
