6b11b4d460bbf00a56d01ed7dc4326b58ac2ed7d
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-03-10 13:50:58 -0800

    initial ws server, using the example as a guide

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,12 +2,38 @@ use actix::{Actor, StreamHandler};
 use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer};
 use actix_web_actors::ws;
 
-async fn index() -> Result<HttpResponse, Error> {
-    let mut builder = actix_web::dev::HttpResponseBuilder::new(actix_web::http::StatusCode::OK);
-    let body = actix_web::dev::Body::from("Hi from websockets!");
-    let response = builder.body(body);
+struct WebsocketServer;
+
+impl Actor for WebsocketServer {
+    type Context = ws::WebsocketContext<Self>;
+}
+
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for WebsocketServer {
+    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
+        match msg {
+            Ok(ws::Message::Ping(msg)) => {
+                println!("ping received!");
+                ctx.pong(&msg);
+            }
+            Ok(ws::Message::Text(text)) => {
+                println!("text recieved!");
+                ctx.text(text);
+            }
+            Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
+            Ok(ws::Message::Close(close)) => {
+                println!("closing...",);
+                ctx.close(close);
+            }
+            _ => (),
+        }
+    }
+}
+
+async fn index(request: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
+    println!("{:?}", &request);
+    let response = ws::start(WebsocketServer {}, &request, stream);
     println!("{:?}", response);
-    Ok(response)
+    response
 }
 
 #[actix_web::main]
