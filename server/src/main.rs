a777e5b0f7c3fa8578498d7d8287d0ce1535b5db
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-03-12 18:11:43 -0800

    experimenting with state and addr

--- server/src/main.rs
+++ server/src/main.rs
@@ -40,7 +40,11 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for WebsocketServer {
     }
 }
 
-async fn echo(request: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
+async fn echo(
+    request: HttpRequest,
+    stream: web::Payload,
+    data: web::Data<std::sync::Mutex<Vec<String>>>,
+) -> Result<HttpResponse, Error> {
     println!("{:?}", &request);
     // let host = &request.headers().get("host").unwrap().to_str().unwrap();
     // let header_value = actix_web::http::HeaderValue::from_str(host).unwrap();
@@ -51,15 +55,40 @@ async fn echo(request: HttpRequest, stream: web::Payload) -> Result<HttpResponse
         .to_str()
         .unwrap();
     let header_value = actix_web::http::HeaderValue::from_str(client).unwrap();
-    let response = ws::start(WebsocketServer { host: header_value }, &request, stream);
-    println!("{:?}", response);
-    response
+    // let response = ws::start(WebsocketServer { host: header_value }, &request, stream);
+    // println!("{:?}", response);
+    // response
+    let sender = ws::start_with_addr(WebsocketServer { host: header_value }, &request, stream);
+
+    match sender {
+        Ok(result) => {
+            let actor = result.0;
+            let message = result.1;
+            let mut managed = data.lock().unwrap();
+
+            managed.push(String::from("hi"));
+
+            println!("{:?}", &managed);
+            println!("connected ? - {:?}", actor.connected());
+            println!("{}", message.status().as_str());
+            Ok(message)
+        }
+        Err(error) => Err(error),
+    }
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/echo/", web::get().to(echo)))
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await
+    let vec: Vec<String> = Vec::new();
+    let app_state = std::sync::Mutex::new(vec);
+    let state = web::Data::new(app_state);
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(state.clone())
+            .route("/echo/", web::get().to(echo))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
