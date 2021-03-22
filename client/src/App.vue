f723710d03c728af28e542c106bae1f43f3d4b8a
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-03-22 13:42:03 -0700

    initial client

---
+++ client/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <h1>Welcome to Your Vue.js + TypeScript App</h1>
+</template>
+
+<script lang="ts">
+const websocketUrl = 'ws://localhost:8888/echo/'
+const websocket = new WebSocket(websocketUrl)
+
+websocket.onmessage = function (onmessage) {
+  console.log(onmessage.data)
+}
+
+websocket.onclose = function (onclose) {
+  console.log(onclose)
+}
+
+websocket.onerror = function (onerror) {
+  console.log(onerror)
+}
+
+function sendMessage (connection: WebSocket) {
+  const message = 'hello!'
+  connection.send(message)
+}
+
+function closeConnection (connection: WebSocket) {
+  const reason = '1000'
+  const message = 'goodbye!'
+  connection.close(reason, message)
+}
+
+console.log(websocket.readyState)
+
+websocket.addEventListener('open', (event) => {
+  console.log(event)
+  sendMessage(websocket)
+})
+
+websocket.addEventListener('message', function (event) {
+  console.log(event)
+  console.log('Message from server ', event.data)
+  closeConnection(websocket)
+})
+</script>
