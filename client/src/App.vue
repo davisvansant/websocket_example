bf05cc2d280d101997b02497da0815018ebeef0a
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-03-24 15:48:55 -0700

    tryout some initial methods, add created"

--- client/src/App.vue
+++ client/src/App.vue
@@ -10,63 +10,76 @@
       font-family:Courier New;
       color: white;"
       >
-      <p style="color:DarkOrange">|||||| websocket client</p>
-      <button style="background-color:black;border:1px solid DarkOrange;padding: 10px 10px;outline:none;margin: 5px 5px;font-family:Courier New;color:DarkOrange;" v-on:click="connecter()">join</button>
-      <p>{{ readystate }}</p>
+      <p style="color:DarkOrange">|||||| websocket client || status [ {{ statuscheck() }} ]</p>
+      <button style="background-color:black;border:1px solid DarkOrange;padding: 10px 10px;outline:none;margin: 5px 5px;font-family:Courier New;color:DarkOrange;" v-on:click="join()">join</button>
+      <button style="background-color:black;border:1px solid DarkOrange;padding: 10px 10px;outline:none;margin: 5px 5px;font-family:Courier New;color:DarkOrange;" v-on:click="statuscheck()">status</button>
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-const websocketUrl = 'ws://localhost:8888/echo/'
-const websocket = new WebSocket(websocketUrl)
-
-websocket.onmessage = function (onmessage) {
-  console.log(onmessage.data)
-}
-
-websocket.onclose = function (onclose) {
-  console.log(onclose)
-}
-
-websocket.onerror = function (onerror) {
-  console.log(onerror)
-}
-
-function sendMessage (connection: WebSocket) {
-  const message = 'hello!'
-  connection.send(message)
-}
-
-function closeConnection (connection: WebSocket) {
-  const reason = 1000
-  const message = 'goodbye!'
-  connection.close(reason, message)
-}
-
-console.log('Websocket status -', websocket.readyState)
-
-websocket.addEventListener('open', (event) => {
-  console.log(event)
-  sendMessage(websocket)
-})
-
-websocket.addEventListener('message', function (event) {
-  console.log(event)
-  console.log('Message from server ', event.data)
-  closeConnection(websocket)
-})
+// const websocketUrl = 'ws://localhost:8888/echo/'
+// const websocket = new WebSocket(websocketUrl)
+//
+// websocket.onmessage = function (onmessage) {
+//   console.log(onmessage.data)
+// }
+//
+// websocket.onclose = function (onclose) {
+//   console.log(onclose)
+// }
+//
+// websocket.onerror = function (onerror) {
+//   console.log(onerror)
+// }
+//
+// function sendMessage (connection: WebSocket) {
+//   const message = 'hello!'
+//   connection.send(message)
+// }
+//
+// function closeConnection (connection: WebSocket) {
+//   const reason = 1000
+//   const message = 'goodbye!'
+//   connection.close(reason, message)
+// }
+//
+// console.log('Websocket status -', websocket.readyState)
+//
+// websocket.addEventListener('open', (event) => {
+//   console.log(event)
+//   sendMessage(websocket)
+// })
+//
+// websocket.addEventListener('message', function (event) {
+//   console.log(event)
+//   console.log('Message from server ', event.data)
+//   closeConnection(websocket)
+// })
 
 export default defineComponent({
   name: 'app',
   data () {
-    return { readystate: 0 | 1 | 2 | 3 }
+    return { connection: WebSocket.prototype, status: 0 | 1 | 2 | 3 }
+  },
+  created () {
+    const websocketUrl = 'ws://localhost:8888/echo/'
+    const websocket:WebSocket = new WebSocket(websocketUrl)
+    console.log('this is happening')
+    this.connection = websocket
+    this.status = websocket.readyState
   },
   methods: {
-    connecter () {
+    join () {
       const websocketUrl = 'ws://localhost:8888/echo/'
-      const websocket = new WebSocket(websocketUrl)
-      this.readystate = websocket.readyState
+      const websocket:WebSocket = new WebSocket(websocketUrl)
+      this.connection = websocket
+    },
+    statuscheck () {
+      const somestatus: WebSocket = this.connection
+      console.log(somestatus.readyState)
+      this.status = somestatus.readyState
+      return this.status
     }
   }
 })
