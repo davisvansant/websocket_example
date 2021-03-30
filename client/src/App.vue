4df076f546d808c8c7cb7d36ee9b5c1e3d00f7f9
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-03-30 13:11:57 -0700

    add dark_orange class to rx_messages

--- client/src/App.vue
+++ client/src/App.vue
@@ -13,7 +13,7 @@
       <p class="dark_orange">|||||| websocket client | {{ url }} |<button class="close_connection_button" v-on:click="close()">CLOSE CONNECTION</button>|</p>
       <p class="dark_orange">||||| connection status |<span class="connection_status">{{ status }}</span>|</p>
       <hr style="height:1px;background-color:DimGrey">
-      <p v-for="message in this.rx_messages" :key="message" style="color:DarkOrange">|<span class="rx_messages">{{ message }}</span></p>
+      <p v-for="message in this.rx_messages" :key="message" class="dark_orange">|<span class="rx_messages">{{ message }}</span></p>
       <input v-model="tx_message" id="tx_message" type="text" placeholder=">" autofocus=true size="110" @keyup.enter="send()"/>
       <button style="position: bottom;background-color:black;border:1px solid DarkOrange;padding: 10px 10px;outline:none;margin: 5px 5px;font-family:Courier New;color:DarkOrange;" v-on:click="send()">send</button>
     </div>
