2ba5f777264f1548c6c89e34d63b1118daa15e9f
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-03-30 12:07:03 -0700

    adjust input to clear on click

--- client/src/App.vue
+++ client/src/App.vue
@@ -14,8 +14,8 @@
       <p style="color:DarkOrange">||||| connection status |<span style="background-color:DarkOrange;padding: 2px 109px;outline:none;margin: 2px 2px;font-family:Courier New;color:Black;">{{ status }}</span>|</p>
       <hr style="height:1px;background-color:DimGrey">
       <p v-for="message in this.rx_messages" :key="message" style="color:DarkOrange">|<span style="background-color:Black;padding: 2px 10px;outline:none;margin: 2px 2px;font-family:Courier New;color:DarkOrange;">{{ message }}</span></p>
-      <input v-model="tx_message" id="tx_message" placeholder=">" autofocus=true size="110"/>
-      <button onclick="document.getElementById('tx_message').value = ''" style="position: bottom;background-color:black;border:1px solid DarkOrange;padding: 10px 10px;outline:none;margin: 5px 5px;font-family:Courier New;color:DarkOrange;" v-on:click="send()">send</button>
+      <input v-model="tx_message" id="tx_message" type="text" placeholder=">" autofocus=true size="110"/>
+      <button style="position: bottom;background-color:black;border:1px solid DarkOrange;padding: 10px 10px;outline:none;margin: 5px 5px;font-family:Courier New;color:DarkOrange;" v-on:click="send()">send</button>
     </div>
 </template>
 
@@ -65,6 +65,9 @@ export default defineComponent({
   methods: {
     send () {
       this.connection.send(this.tx_message)
+      this.tx_message = ''
+
+      return this.tx_message
     },
     dosomething (something: string) {
       this.rx_messages.push(something)
