d486e8a79cf84b9f579d02192acb11c78e6c59c6
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-03-30 12:54:18 -0700

    initial close_connection_button style and class usage

--- client/src/App.vue
+++ client/src/App.vue
@@ -10,7 +10,7 @@
       font-family:Courier New;
       color: white;"
       >
-      <p class="dark_orange">|||||| websocket client | {{ url }} |<button style="background-color:Black;border:1px solid DarkOrange;padding: 5px 75px;outline:none;margin: 5px 5px;font-family:Courier New;color:PapayaWhip;" v-on:click="close()">CLOSE CONNECTION</button>|</p>
+      <p class="dark_orange">|||||| websocket client | {{ url }} |<button class="close_connection_button" v-on:click="close()">CLOSE CONNECTION</button>|</p>
       <p class="dark_orange">||||| connection status |<span style="background-color:DarkOrange;padding: 2px 109px;outline:none;margin: 2px 2px;font-family:Courier New;color:Black;">{{ status }}</span>|</p>
       <hr style="height:1px;background-color:DimGrey">
       <p v-for="message in this.rx_messages" :key="message" style="color:DarkOrange">|<span style="background-color:Black;padding: 2px 10px;outline:none;margin: 2px 2px;font-family:Courier New;color:DarkOrange;">{{ message }}</span></p>
@@ -105,5 +105,14 @@ export default defineComponent({
   }
   .dark_orange {
     color: DarkOrange;
+  }
+  .close_connection_button {
+    background-color:Black;
+    border:1px solid DarkOrange;
+    padding: 5px 75px;
+    outline:none;
+    margin: 5px 5px;
+    font-family:Courier New;
+    color:PapayaWhip;
   }
 </style>
